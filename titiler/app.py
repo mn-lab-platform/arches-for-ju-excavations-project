5c2ec8fa04723d8d2e8bd56cc3a6c82af16dcb3c
Maciej Mirowski <maciej.mir@wp.pl>
2026-02-19 16:55:08 +0100

    iiif raport v1 without height and cords

--- titiler/app.py
+++ titiler/app.py
@@ -1,7 +1,7 @@
-from titiler.core.factory import TilerFactory
-from titiler.image.factory import IIIFFactory, LocalTilerFactory, MetadataFactory, TilerFactory
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from titiler.core.factory import TilerFactory as CoreTilerFactory
+from titiler.image.factory import IIIFFactory, LocalTilerFactory, MetadataFactory
 
 app = FastAPI(title="titiler + titiler-image")
 
@@ -13,26 +13,21 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Metadata 
 meta = MetadataFactory()
 app.include_router(meta.router, tags=["Metadata"])
 
-# COG 
-cog = TilerFactory()
+cog = CoreTilerFactory()
 app.include_router(cog.router, prefix="/cog", tags=["COG"])
 
-# IIIF 
-iiif = IIIFFactory(router_prefix="/iiif")
+iiif = IIIFFactory()
 app.include_router(iiif.router, prefix="/iiif", tags=["IIIF"])
 
-# 90% sure its useless 
-local = LocalTilerFactory(router_prefix="/image")
-app.include_router(local.router, prefix="/image", tags=["Local Tiles"])
-#tms
-local = TilerFactory(router_prefix="/tiles")
-app.include_router(local.router, prefix="/tiles", tags=["TMS TILES"])
+local_image = LocalTilerFactory()
+app.include_router(local_image.router, prefix="/image", tags=["Local Tiles"])
+
+tiles = CoreTilerFactory()
+app.include_router(tiles.router, prefix="/tiles", tags=["TMS Tiles"])
 
 @app.get("/healthz")
 def health():
-    """Health check."""
     return {"ping": "pong!"}
